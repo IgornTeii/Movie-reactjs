21204b766c448410359bf4f5dc225550b67b3b0a
Igor <igornteixeira48@gmail.com>
2023-08-01 23:18:13 -0300

    starting-to-style

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg" alt="Spider-Man"/>
+                    <span>Spider-Man</span>
+                </li>
+                <li>
+                    <img src="https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg" alt="Spider-Man"/>
+                    <span>Spider-Man</span>
+                </li>
+                <li>
+                    <img src="https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg" alt="Spider-Man"/>
+                    <span>Spider-Man</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
